07d5ec61a8af79d3d9fd046352c635dd1fe4fca5
张洋 <zhangyang1@peersafe.cn>
2020-07-05 15:45:27 +0800

    fix(api): + fix the code of addTwoNumbers and twoSum.

--- addTwoNumbers.go
+++ addTwoNumbers.go
@@ -40,3 +40,120 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	return head.Next
 }
+
+/*
+1. 单链表
+1.1. 定义
+单向链表（单链表）是链表的一种，其特点是链表的链接方向是单向的，对链表的访问要通过顺序读取从头部开始；链表是使用指针进行构造的列表；又称为结点列表，因为链表是由一个个结点组装起来的；其中每个结点都有指针成员变量指向列表中的下一个结点；
+列表是由结点构成，head指针指向第一个成为表头结点，而终止于最后一个指向nuLL的指针。
+
+1.2. 优点
+单个结点创建非常方便，普通的线性内存通常在创建的时候就需要设定数据的大小
+结点的删除非常方便，不需要像线性结构那样移动剩下的数据
+结点的访问方便，可以通过循环或者递归的方法访问到任意数据，但是平均的访问效率低于线性表。
+*/
+
+/*
+//链表的相关操作
+//单链表 -- 线性表
+//定义节点
+type Node struct {
+	Data int
+	Next *Node
+}
+
+//返回第一个节点
+//h 头结点
+func GetFirst(h *Node) *Node {
+	if h.Next == nil {
+		return nil
+	}
+	return h.Next
+}
+
+//返回最后一个节点
+//h 头结点
+func GetLast(h *Node) *Node {
+	if h.Next == nil {
+		return nil
+	}
+	i := h
+	for i.Next != nil {
+		i = i.Next
+		if i.Next == nil {
+			return i
+		}
+	}
+	return nil
+}
+
+//取长度
+func GetLength(h *Node) int {
+	var i int = 0
+	n := h
+	for n.Next != nil {
+		i++
+		n = n.Next
+	}
+	return i
+}
+
+//插入一个节点
+//h: 头结点
+//d:要插入的节点
+//p:要插入的位置
+func Insert(h, d *Node, p int) bool {
+	if h.Next == nil {
+		h.Next = d
+		return true
+	}
+	i := 0
+	n := h
+	for n.Next != nil {
+		i++
+		if i == p {
+			n.Next = d
+			return true
+		}
+	}
+	n = n.Next
+	if n.Next == nil {
+		n.Next = d
+		return true
+	}
+	return false
+}
+
+//取出指定节点
+func GetLoc(h *Node, p int) *Node {
+	if p < 0 || p > GetLength(h) {
+		return nil
+	}
+	var i int = 0
+	n := h
+	for n.Next != nil {
+		i++
+		n = n.Next
+		if i == p {
+			return n
+		}
+	}
+	return nil
+}
+
+func main() {
+	//初始化一个头结点
+	var h Node
+	//往链表插入10个元素
+	for i := 1; i <= 10; i++ {
+		var d Node
+		d.Data = i
+		Insert(&h, &d, i)
+		fmt.Println(GetLoc(&h, i))
+	}
+	fmt.Println(GetLength(&h))
+	fmt.Println(GetFirst(&h))
+	fmt.Println(GetLast(&h))
+	fmt.Println(GetLoc(&h, 6))
+}
+*/
--- twoSum.go
+++ twoSum.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
 /*
 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -10,46 +13,35 @@ import "fmt"
 所以返回 [0, 1]
 */
 
-//func main() {
-//	for {
-//		var target int
-//		result := make([]int, 0)
-//		value := make([]int, 0)
-//		nums := []int{3, 3}
-//
-//		_, err := fmt.Scanf("%d", &target)
-//		if err != nil {
-//			fmt.Errorf("scanf err : %v\n", err)
-//			return
-//		}
-//		if target == 0 {
-//			fmt.Printf("程序退出")
-//			os.Exit(0)
-//		}
-//		var flag bool
-//		for key1, num1 := range nums {
-//			for key2, num2 := range nums {
-//				if (num1+num2 == target && num1 == num2 && key1 != key2) || (num1+num2 == target && num1 != num2) {
-//					result = append(result, key1, key2)
-//					value = append(value, num1, num2)
-//					flag = true
-//					break
-//				}
-//			}
-//			if flag == true {
-//				break
-//			}
-//		}
-//
-//		fmt.Printf("result = %v\n", result)
-//		fmt.Printf("value = %v\n", value)
-//	}
-//}
 func main() {
-	nums := []int{3,2,4,3}
+	nums := []int{3, 2, 4, 5}
 	target := 6
-	fmt.Printf("%v\n", twoSum2(nums, target))
+	fmt.Printf("twoSum1 %v\n", twoSum1(nums, target))
+	fmt.Printf("twoSum2 %v\n", twoSum2(nums, target))
 }
+
+//twoSum1和twoSum2是两种方法，方法1消耗内存会比较小，但是比较麻烦；方法二消耗内存大，但是代码简单，不易懂
+func twoSum1(nums []int, target int) []int {
+	result := make([]int, 0)
+	value := make([]int, 0)
+
+	var flag bool
+	for key1, num1 := range nums {
+		for key2, num2 := range nums {
+			if (num1+num2 == target && num1 == num2 && key1 != key2) || (num1+num2 == target && num1 != num2) {
+				result = append(result, key1, key2)
+				value = append(value, num1, num2)
+				flag = true
+				break
+			}
+		}
+		if flag == true {
+			break
+		}
+	}
+	return result
+}
+
 func twoSum2(nums []int, target int) []int {
 	m := map[int]int{}
 	for i, v := range nums {
