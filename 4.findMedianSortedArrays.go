de5d40c40725d788b740bbf02588c946c7c986cb
张洋 <zhangyang1@peersafe.cn>
2020-07-08 22:02:13 +0800

    fix(api):+ add the subject for 4.findMedianSortedArrays.go.

--- 4.findMedianSortedArrays.go
+++ 4.findMedianSortedArrays.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	//nums1 := []int{1, 3}
-	//nums2 := []int{2}
-	//fmt.Println(findMedianSortedArrays(nums1, nums2))
-	fmt.Println("max : ", int(^uint(0)>>1))
-	fmt.Println("min : ", ^int(^uint(0)>>1))
-	fmt.Println("uint(0) : ", (^uint(0)>>1))
+/*
+4. 寻找两个正序数组的中位数
+给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+你可以假设 nums1 和 nums2 不会同时为空。
 
+示例 1:
+nums1 = [1, 3]
+nums2 = [2]
+则中位数是 2.0
+
+示例 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+则中位数是 (2 + 3)/2 = 2.5
+*/
+
+func main() {
+	nums1 := []int{1, 3}
+	nums2 := []int{2}
+	fmt.Println(findMedianSortedArrays(nums1, nums2))
 }
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	//将最短的数组作为nums1
